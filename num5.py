5ee3a21a453028e53e3d73bced9b6abcb6fe71e4
Amletick <VadimV2001@gmail.com>
2024-09-04 17:44:53 +0300

    hw4

--- num5.py
+++ num5.py
@@ -1,6 +1,6 @@
 word1 = input("Первое слово: ")
 word2 = input("Второе слово: ")
 if sorted(word1.lower()) == sorted(word2.lower()):
-    print("Анаграмма")
+    print("Это анаграмма")
 else:
-    print("Не анаграмма")
+    print("Это не анаграмма")
